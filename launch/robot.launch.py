c785c1d2d5384d6c3b4f715114a806c98f4d59d3
Artyks03 <horama57@gmail.com>
2025-03-11 16:20:24 +0100

    real

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,24 +1,26 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
+
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
 
-    package_name='autobot' 
+    package_name='robotek' 
 
-    # Spousti robot_state_publisher (rsp.launch.py) + sim_time false
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false'}.items()
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
     rplidar = IncludeLaunchDescription(
@@ -32,7 +34,7 @@ def generate_launch_description():
                      get_package_share_directory(package_name),'launch','camera.launch.py'
                  )])
      )
- 
+
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'params','twist_mux.yaml')
     twist_mux = Node(
             package="twist_mux",
@@ -41,20 +43,19 @@ def generate_launch_description():
             remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
         )
 
-    # CONTROLLER MANAGER u realneho robota a jeho kontrolery -> musime ho spustit sami
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params= os.path.join(get_package_share_directory(package_name),'params','my_controllers.yaml')
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'params','my_controllers.yaml')
 
     controller_manager= Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[{'robot_description': robot_description},
-                    controller_params]
+                    controller_params_file]
     )
     
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
-    
+
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -82,17 +83,13 @@ def generate_launch_description():
     )
 
     
-
-    ld = LaunchDescription()
-
-    ld.add_action(rsp)
-    ld.add_action(rplidar)
-    ld.add_action(camera)
-    ld.add_action(delayed_controller_manager)
-    #ld.add_action(diff_drive_spawner)
-    #ld.add_action(joint_broad_spawner)
-    ld.add_action(delayed_diff_drive_spawner)
-    ld.add_action(delayed_joint_broad_spawner)
-
-    return ld
-  
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        twist_mux,
+        rplidar,
+        camera,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner
+    ])
